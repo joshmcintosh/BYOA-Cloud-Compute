4f0570e4da21307c75a005aa54e9c6330d4629cb
eetar1 <ekwan14@gmail.com>
2019-03-15 15:47:21 -0700

    Created a script to fetch data from a index webpage

---
+++ app/fetchData/fetchData.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def fetchData(url):
+    links = []
+    resp = requests.get(url)
+    soup = BeautifulSoup("\n".join(resp.text.splitlines()[2:]), "html.parser")
+    for i in soup.find_all("a"):
+        links.append(i.get("href"))
+    del links[0]
+    return links
+
+
+# https://landsat-pds.s3.amazonaws.com/c1/L8/139/045/LC08_L1TP_139045_20170304_20170316_01_T1/LC08_L1TP_139045_20170304_20170316_01_T1_B10_wrk.IMD
+
+
+def main():
+    url = "https://landsat-pds.s3.amazonaws.com/c1/L8/139/045/LC08_L1TP_139045_20170304_20170316_01_T1/index.html"
+
+    data = fetchData(url)
+
+    while url[-1] != "/":
+        url = url[:-1]
+
+    saveData(data, url)
+
+
+def saveData(extensions, url):
+    for ext in extensions:
+        resp = requests.get(url + ext, allow_redirects=True)
+        open(ext, "wb").write(resp.content)
+
+
+if __name__ == "__main__":
+    main()
