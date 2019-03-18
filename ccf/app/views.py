aacae0c8fdd055a95270f684d2ff58baf2d48bce
joshmcintosh <46505027+joshmcintosh@users.noreply.github.com>
2019-03-18 13:06:00 -0700

    GH 14 Account Creation (#44)
    * GH-14 Added initial functionality
    
    * GH-14 Added signup button to login page.
    
    * GH-14 Formatting with Black.
    
    * GH-14 Gotta isort too.
    
    * GH-14 Changes to unit tests
    
    * GH-14 Finished unit tests and simpilfied structure so separate url.py etc not needed.
    
    * GH-14 Moved some files after merge from master
    
    * GH-14 Fixing and reorganizing after GH-23
    
    * GH-14 Added test for logged in user directing to home page.
    
    * GH-14 Changes as per code review and deleted leftover files during dev.
    
    * GH-14 Added comments to views.py as per review.

--- ccf/app/views.py
+++ ccf/app/views.py
@@ -1,17 +1,26 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
 
 @login_required
-# Create your views here.
 def homepage_view(request):
+    """Homepage to Login Redirect
+
+    Returns to the user the homepage template if they are logged in, otherwise redirects them
+    to the login page.
+    """
     return render(request, "tmp_home.html")
 
 
 class SignUp(generic.CreateView):
+    """Sign-up Page
+
+    Returns to the user the sign-up template.
+    """
+
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
