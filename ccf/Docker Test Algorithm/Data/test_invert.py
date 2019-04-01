9a72dc4d32cfde7727198aad7b2035a89a5fadf5
eetar1 <ekwan14@gmail.com>
2019-04-01 16:16:37 -0700

    renamed file to testImage.jps to make testing easier

--- ccf/Docker Test Algorithm/Data/test_invert.py
+++ ccf/Docker Test Algorithm/Data/test_invert.py
@@ -6,6 +6,6 @@ import invert
 
 
 def test_createImage():
-    invert.process(["3959X_2017-3.jpg"])
+    invert.process(["testImage.jpg"])
     assert os.path.isfile("fixed.jpeg")
     os.remove("fixed.jpeg")
