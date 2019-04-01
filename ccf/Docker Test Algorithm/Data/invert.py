32032f3dedb96dde8c51122f96b344bf11d3153b
eetar1 <ekwan14@gmail.com>
2019-04-01 15:55:44 -0700

    reformatted invert.py

--- ccf/Docker Test Algorithm/Data/invert.py
+++ ccf/Docker Test Algorithm/Data/invert.py
@@ -1,17 +1,20 @@
-from PIL import Image
-import PIL.ImageOps
 import sys
+
+import PIL.ImageOps
+from PIL import Image
 
 
 def process(name):
     print(name)
     image = Image.open(str(name[0]))
     out = PIL.ImageOps.invert(image)
-    out.save('fixed.jpeg')
+    out.save("fixed.jpeg")
+
 
 def main():
     name = sys.argv[1:]
     process(name)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
