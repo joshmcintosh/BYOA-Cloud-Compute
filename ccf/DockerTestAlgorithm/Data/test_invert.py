28ce4ff32bd594d265764b353e29e188d2583a8f
eetar1 <ekwan14@gmail.com>
2019-04-04 17:58:33 -0700

    Added home symbol to hopefully fix path issue

--- ccf/DockerTestAlgorithm/Data/test_invert.py
+++ ccf/DockerTestAlgorithm/Data/test_invert.py
@@ -6,6 +6,6 @@ import invert
 
 
 def test_createImage():
-    invert.process(["ccf/DockerTestAlgorithm/Data/testImage.jpg"])
-    assert os.path.isfile("ccf/DockerTestAlgorithm/Data/fixed.jpeg")
+    invert.process(["~/ccf/DockerTestAlgorithm/Data/testImage.jpg"])
+    assert os.path.isfile("fixed.jpeg")
     os.remove("fixed.jpeg")
