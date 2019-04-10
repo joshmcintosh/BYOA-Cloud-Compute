0ce4b36b51c0132732fbb53176a29335091c516e
eetar1 <ekwan14@gmail.com>
2019-04-10 16:43:54 -0700

    Change image encoding

--- ccf/app/settings/base.py
+++ ccf/app/settings/base.py
@@ -28,7 +28,6 @@ except ImportError:
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = BASE_DIR+"/.process"
-MEDIA_URL = '/media/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
