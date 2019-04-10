2c7f37f1417145083d376cbbca5609c350c3c1d3
Ross Alexandra <rossandus@hotmail.com>
2019-04-10 16:03:40 -0700

    finalPush - finished jobs can now be seen from admin panel.

--- ccf/app/admin.py
+++ ccf/app/admin.py
@@ -1,5 +1,6 @@
-from app.models import Job
+from app.models import FinishedJob, Job
 from django.contrib import admin
 
 # Allow the Django admin to manage the Jobs database.
 admin.site.register(Job)
+admin.site.register(FinishedJob)
